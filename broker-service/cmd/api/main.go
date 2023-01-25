3de7280b2f77b6028e9f949ac3910fda2c1a99ef
Alireza Jahanshahloo <a.jahanshahlo@gmail.com>
2023-01-25 15:32:35 +0330

    change rabbitMQ connection

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -18,6 +18,7 @@ type Config struct {
 }
 
 func main() {
+	// try to connect to rabbitmq
 	rabbitConn, err := connect()
 	if err != nil {
 		log.Println(err)
@@ -49,15 +50,14 @@ func connect() (*amqp.Connection, error) {
 	var backOff = 1 * time.Second
 	var connection *amqp.Connection
 
+	// don't continue until rabbit is ready
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
-			fmt.Println("Rabbitmq not yet ready...")
+			fmt.Println("RabbitMQ not yet ready...")
 			counts++
-
 		} else {
-
-			log.Println("Connected to rabbitMQ")
+			log.Println("Connected to RabbitMQ!")
 			connection = c
 			break
 		}
@@ -66,10 +66,12 @@ func connect() (*amqp.Connection, error) {
 			fmt.Println(err)
 			return nil, err
 		}
-		backOff = time.Duration(math.Pow(float64(counts), 2))
-		log.Println("Backing off... ")
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
 		time.Sleep(backOff)
 		continue
 	}
+
 	return connection, nil
 }
--- listener-service/main.go
+++ listener-service/main.go
@@ -12,22 +12,28 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-type Config struct {
-}
+//type Config struct {
+//}
 
 func main() {
+	// try to connect to rabbitmq
 	rabbitConn, err := connect()
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 	defer rabbitConn.Close()
+
+	// start listening for messages
 	log.Println("Listening for and consuming RabbitMQ messages...")
 
+	// create consumer
 	consumer, err := event.NewConsumer(rabbitConn)
 	if err != nil {
 		panic(err)
 	}
+
+	// watch the queue and consume events
 	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
 	if err != nil {
 		log.Println(err)
@@ -38,15 +44,14 @@ func connect() (*amqp.Connection, error) {
 	var backOff = 1 * time.Second
 	var connection *amqp.Connection
 
+	// don't continue until rabbit is ready
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
-			fmt.Println("Rabbitmq not yet ready...")
+			fmt.Println("RabbitMQ not yet ready...")
 			counts++
-
 		} else {
-
-			log.Println("Connected to rabbitMQ")
+			log.Println("Connected to RabbitMQ!")
 			connection = c
 			break
 		}
@@ -55,10 +60,12 @@ func connect() (*amqp.Connection, error) {
 			fmt.Println(err)
 			return nil, err
 		}
-		backOff = time.Duration(math.Pow(float64(counts), 2))
-		log.Println("Backing off... ")
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
 		time.Sleep(backOff)
 		continue
 	}
+
 	return connection, nil
 }
